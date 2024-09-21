cc990664fabb3fd0b7148742e66f34efb82844f6
Toby16 <bolajibaasit14@gmail.com>
2024-09-21 04:26:33 -0400

    555 PAYMENT | Report:

--- PAYMENT/pydantic_models.py
+++ PAYMENT/pydantic_models.py
@@ -14,10 +14,36 @@ class paystack_model(BaseModel):
 
 
 # FLUTTERWAVE PAYMENT
-class flutterwave_model(BaseModel):
-    email: str = Field(examples=["test@example.com"]) 
-    amount: str = Field(examples=["5000"])
+class flutterwave_weekly_model(BaseModel):
+    email: str = Field(examples=["test@example.com"])
+    amount: str = Field(examples=["1500"])
     currency: str = Field(examples=["NGN"])
     redirect_url: str = Field(examples=["https://google.com/"])
     first_name: str = Field(examples=["test_firstname"])
     last_name: str = Field(examples=["test_lastname"])
+
+class flutterwave_monthly_model(BaseModel):
+    email: str = Field(examples=["test@example.com"]) 
+    amount: str = Field(examples=["4900"])
+    currency: str = Field(examples=["NGN"])
+    redirect_url: str = Field(examples=["https://google.com/"])
+    first_name: str = Field(examples=["test_firstname"])
+    last_name: str = Field(examples=["test_lastname"])
+
+class flutterwave_biannual_model(BaseModel):
+    email: str = Field(examples=["test@example.com"])
+    amount: str = Field(examples=["24900"])
+    currency: str = Field(examples=["NGN"])
+    redirect_url: str = Field(examples=["https://google.com/"])
+    first_name: str = Field(examples=["test_firstname"])
+    last_name: str = Field(examples=["test_lastname"])
+
+class flutterwave_yearly_model(BaseModel):
+    email: str = Field(examples=["test@example.com"])
+    amount: str = Field(examples=["40000"])
+    currency: str = Field(examples=["NGN"])
+    redirect_url: str = Field(examples=["https://google.com/"])
+    first_name: str = Field(examples=["test_firstname"])
+    last_name: str = Field(examples=["test_lastname"])
+
+
--- PAYMENT/routes.py
+++ PAYMENT/routes.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from PAYMENT.pydantic_models import (
-    flutterwave_model, paystack_model,
-    verify_paystack_payment_model
+    flutterwave_yearly_model, flutterwave_biannual_model,
+    flutterwave_monthly_model, flutterwave_weekly_model,
+    paystack_model, verify_paystack_payment_model
 )
 
 
@@ -25,8 +26,8 @@ PAYSTACK_BASE_URL = "https://api.paystack.co/transaction"
 
 per_dollar = 1400
 
-@app.post("/payment/paystack/weekly", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
-@app.post("/payment/paystack/weekly/", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
+@app.post("/payment/paystack/weekly", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
+@app.post("/payment/paystack/weekly/", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
 def create_payment_paystack(data: paystack_model):
     #return PAYSTACK_SECRET_KEY
     headers = {
@@ -63,8 +64,8 @@ def create_payment_paystack(data: paystack_model):
         "response": response.json()
     }
 
-@app.post("/payment/paystack/monthly", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
-@app.post("/payment/paystack/monthly/", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
+@app.post("/payment/paystack/monthly", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
+@app.post("/payment/paystack/monthly/", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
 def create_payment_paystack(data: paystack_model):
     headers = {
         "Authorization": f"Bearer {PAYSTACK_SECRET_KEY}",
@@ -102,8 +103,8 @@ def create_payment_paystack(data: paystack_model):
 
 
 
-@app.post("/payment/paystack/biannually", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
-@app.post("/payment/paystack/biannually/", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
+@app.post("/payment/paystack/biannually", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
+@app.post("/payment/paystack/biannually/", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
 def create_payment_paystack(data: paystack_model):
     headers = {
         "Authorization": f"Bearer {PAYSTACK_SECRET_KEY}",
@@ -141,8 +142,8 @@ def create_payment_paystack(data: paystack_model):
 
 
 
-@app.post("/payment/paystack/yearly", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
-@app.post("/payment/paystack/yearly/", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
+@app.post("/payment/paystack/yearly", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
+@app.post("/payment/paystack/yearly/", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
 def create_payment_paystack(data: paystack_model):
     headers = {
         "Authorization": f"Bearer {PAYSTACK_SECRET_KEY}",
@@ -183,8 +184,8 @@ def create_payment_paystack(data: paystack_model):
 
 
 # [ VERIFY PAYSTACK PAYMENT ]
-@app.post("/payment/paystack/verify", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
-@app.post("/payment/paystack/verify/", status_code=status.HTTP_200_OK, tags=["PAYMENT"])
+@app.post("/payment/paystack/verify", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
+@app.post("/payment/paystack/verify/", status_code=status.HTTP_200_OK, tags=["PAYSTACK PAYMENT"])
 def verify_paystack_payment(data: verify_paystack_payment_model):
     headers = {
         "Authorization": f"Bearer {PAYSTACK_SECRET_KEY}"
@@ -226,9 +227,9 @@ def verify_paystack_payment(data: verify_paystack_payment_model):
 FLW_SECRET_KEY = os.getenv('FLW_SECRET_KEY')
 FLW_BASE_URL = 'https://api.flutterwave.com/v3'
 
-@app.post("/payment/flutterwave", status_code=status.HTTP_200_OK, tags=["PENDING"])
-@app.post("/payment/flutterwave/", status_code=status.HTTP_200_OK, tags=["PENDING"])
-def create_payment_flutterwave(data: flutterwave_model):
+@app.post("/payment/flutterwave/weekly", status_code=status.HTTP_200_OK, tags=["FLUTTERWAVE PAYMENT"])
+@app.post("/payment/flutterwave/weekly/", status_code=status.HTTP_200_OK, tags=["FLUTTERWAVE PAYMENT"])
+def create_payment_flutterwave(data: flutterwave_weekly_model):
     headers = {
         "Authorization": f"Bearer {FLW_SECRET_KEY}",
         "Content-Type": "application/json"
@@ -237,8 +238,6 @@ def create_payment_flutterwave(data: flutterwave_model):
     data = data.dict()
     data["tx_ref"] = "REF-{}".format(randint(100000000, 999999999))
     data["currency"] = "NGN"
-    # data["currency"] = "USD"
-    # data["payment_options"] = "card, account, googlepay, applepay"
     data["amount"] = str(data["amount"])
 
     amount_format = ""
@@ -259,11 +258,9 @@ def create_payment_flutterwave(data: flutterwave_model):
     # flutterwave payment payload
     payload = {
         "tx_ref": data["tx_ref"],
-        # "amount": int(amount_format) * per_dollar,
         "amount": int(amount_format),
         "currency": data["currency"],
         "redirect_url": data["redirect_url"],
-        # "payment_options": data["payment_options"],
         "customer": {
             "email": data["email"],
             "name": "{} {}".format(data["first_name"], data["last_name"])
@@ -272,7 +269,7 @@ def create_payment_flutterwave(data: flutterwave_model):
             "title": "Test Inc.",
             "logo": "https://luravpn.nyc3.digitaloceanspaces.com/country_icon/.misc/1731.png"
         },
-        "payment_plan": "127448"
+        "payment_plan": "127448"  # for #1,500
     }
 
 
@@ -280,6 +277,7 @@ def create_payment_flutterwave(data: flutterwave_model):
         with httpx.Client(timeout=Timeout(50.0)) as client:
             # set timeout to 30 seconds
             response = client.post(f"{FLW_BASE_URL}/payments", json=payload, headers=headers)
+            # response = client.get(f"{FLW_BASE_URL}/payment-plans", headers=headers)
     except httpx.TimeoutException as e:
         raise HTTPException(status_code=500, detail=str(e))
     except Exception as e:
@@ -288,7 +286,6 @@ def create_payment_flutterwave(data: flutterwave_model):
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=response.json())
         
-    # return PaymentResponse(status="success", message="Payment created successfully", data=response.json())
     return {
         "statusCode": 200,
         "message": "Payment created successfully",
