8ce1086123564367db56f6dfb50d12e4d3d1f5fe
Toby16 <bolajibaasit14@gmail.com>
2024-09-21 04:54:56 -0400

    555 PAYMENT | Report:

--- PAYMENT/routes.py
+++ PAYMENT/routes.py
@@ -291,3 +291,200 @@ def create_payment_flutterwave(data: flutterwave_weekly_model):
         "message": "Payment created successfully",
         "data": (response.json())["data"]
     }
+
+
+@app.post("/payment/flutterwave/monthly", status_code=status.HTTP_200_OK, tags=["FLUTTERWAVE PAYMENT"])
+@app.post("/payment/flutterwave/monthly/", status_code=status.HTTP_200_OK, tags=["FLUTTERWAVE PAYMENT"])
+def create_payment_flutterwave(data: flutterwave_monthly_model):
+    headers = {
+        "Authorization": f"Bearer {FLW_SECRET_KEY}",
+        "Content-Type": "application/json"
+    }
+    
+    data = data.dict()
+    data["tx_ref"] = "REF-{}".format(randint(100000000, 999999999))
+    data["currency"] = "NGN"
+    data["amount"] = str(data["amount"])
+
+    amount_format = ""
+
+    if "," in data["amount"]:
+        amount_format = (data["amount"]).replace(",","")
+    elif (data["amount"]).endswith(".00"):
+        amount_format = data["amount"][:-3]
+
+    if amount_format.endswith(".00"):
+        amount_format = amount_format[:-3]
+    elif "," in amount_format:
+        amount_format = (amount_format).replace(",","")
+
+    if len(amount_format) <= 0:
+        amount_format = data["amount"]
+    
+    # flutterwave payment payload
+    payload = {
+        "tx_ref": data["tx_ref"],
+        "amount": int(amount_format),
+        "currency": data["currency"],
+        "redirect_url": data["redirect_url"],
+        "customer": {
+            "email": data["email"],
+            "name": "{} {}".format(data["first_name"], data["last_name"])
+        },
+        "customizations": {
+            "title": "Test Inc.",
+            "logo": "https://luravpn.nyc3.digitaloceanspaces.com/country_icon/.misc/1731.png"
+        },
+        "payment_plan": "127449"  # for #4,900
+    }
+
+
+    try:
+        with httpx.Client(timeout=Timeout(50.0)) as client:
+            # set timeout to 30 seconds
+            response = client.post(f"{FLW_BASE_URL}/payments", json=payload, headers=headers)
+            # response = client.get(f"{FLW_BASE_URL}/payment-plans", headers=headers)
+    except httpx.TimeoutException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+        
+    return {
+        "statusCode": 200,
+        "message": "Payment created successfully",
+        "data": (response.json())["data"]
+    }
+
+
+@app.post("/payment/flutterwave/biannually", status_code=status.HTTP_200_OK, tags=["FLUTTERWAVE PAYMENT"])
+@app.post("/payment/flutterwave/biannually/", status_code=status.HTTP_200_OK, tags=["FLUTTERWAVE PAYMENT"])
+def create_payment_flutterwave(data: flutterwave_biannual_model):
+    headers = {
+        "Authorization": f"Bearer {FLW_SECRET_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    data = data.dict()
+    data["tx_ref"] = "REF-{}".format(randint(100000000, 999999999))
+    data["currency"] = "NGN"
+    data["amount"] = str(data["amount"])
+
+    amount_format = ""
+
+    if "," in data["amount"]:
+        amount_format = (data["amount"]).replace(",","")
+    elif (data["amount"]).endswith(".00"):
+        amount_format = data["amount"][:-3]
+
+    if amount_format.endswith(".00"):
+        amount_format = amount_format[:-3]
+    elif "," in amount_format:
+        amount_format = (amount_format).replace(",","")
+
+    if len(amount_format) <= 0:
+        amount_format = data["amount"]
+
+    # flutterwave payment payload
+    payload = {
+        "tx_ref": data["tx_ref"],
+        "amount": int(amount_format),
+        "currency": data["currency"],
+        "redirect_url": data["redirect_url"],
+        "customer": {
+            "email": data["email"],
+            "name": "{} {}".format(data["first_name"], data["last_name"])
+        },
+        "customizations": {
+            "title": "Test Inc.",
+            "logo": "https://luravpn.nyc3.digitaloceanspaces.com/country_icon/.misc/1731.png"
+        },
+        "payment_plan": "127450"  # for #24,900
+    }
+
+
+    try:
+        with httpx.Client(timeout=Timeout(50.0)) as client:
+            # set timeout to 30 seconds
+            response = client.post(f"{FLW_BASE_URL}/payments", json=payload, headers=headers)
+            # response = client.get(f"{FLW_BASE_URL}/payment-plans", headers=headers)
+    except httpx.TimeoutException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+
+    return {
+        "statusCode": 200,
+        "message": "Payment created successfully",
+        "data": (response.json())["data"]
+    }
+
+@app.post("/payment/flutterwave/yearly", status_code=status.HTTP_200_OK, tags=["FLUTTERWAVE PAYMENT"])
+@app.post("/payment/flutterwave/yearly/", status_code=status.HTTP_200_OK, tags=["FLUTTERWAVE PAYMENT"])
+def create_payment_flutterwave(data: flutterwave_yearly_model):
+    headers = {
+        "Authorization": f"Bearer {FLW_SECRET_KEY}",
+        "Content-Type": "application/json"
+    }
+    
+    data = data.dict()
+    data["tx_ref"] = "REF-{}".format(randint(100000000, 999999999))
+    data["currency"] = "NGN"
+    data["amount"] = str(data["amount"])
+
+    amount_format = ""
+
+    if "," in data["amount"]:
+        amount_format = (data["amount"]).replace(",","")
+    elif (data["amount"]).endswith(".00"):
+        amount_format = data["amount"][:-3]
+
+    if amount_format.endswith(".00"):
+        amount_format = amount_format[:-3]
+    elif "," in amount_format:
+        amount_format = (amount_format).replace(",","")
+
+    if len(amount_format) <= 0:
+        amount_format = data["amount"]
+    
+    # flutterwave payment payload
+    payload = {
+        "tx_ref": data["tx_ref"],
+        "amount": int(amount_format),
+        "currency": data["currency"],
+        "redirect_url": data["redirect_url"],
+        "customer": {
+            "email": data["email"],
+            "name": "{} {}".format(data["first_name"], data["last_name"])
+        },
+        "customizations": {
+            "title": "Test Inc.",
+            "logo": "https://luravpn.nyc3.digitaloceanspaces.com/country_icon/.misc/1731.png"
+        },
+        "payment_plan": "127451"  # for #40,000
+    }
+
+
+    try:
+        with httpx.Client(timeout=Timeout(50.0)) as client:
+            # set timeout to 30 seconds
+            response = client.post(f"{FLW_BASE_URL}/payments", json=payload, headers=headers)
+            # response = client.get(f"{FLW_BASE_URL}/payment-plans", headers=headers)
+    except httpx.TimeoutException as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+        
+    return {
+        "statusCode": 200,
+        "message": "Payment created successfully",
+        "data": (response.json())["data"]
+    }
